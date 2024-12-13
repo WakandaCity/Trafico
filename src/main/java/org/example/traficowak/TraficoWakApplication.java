6c8468a029081c3b07c93d7545d9d5d56fc5c6c2
franlopezsoto <flopesot@myuax.com>
2024-12-13 19:10:09 +0100

    hell

---
+++ src/main/java/org/example/traficowak/TraficoWakApplication.java
@@ -0,0 +1,13 @@
+package org.example.traficowak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraficoWakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraficoWakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/traficowak/TraficoWakApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.traficowak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraficoWakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
